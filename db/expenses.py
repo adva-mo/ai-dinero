6ab6ed89d3a328fc7e00df0414e1c6f60f176a43
adva <advamozes@gmail.com>
2024-11-01 20:39:34 +0200

    with full expenses CRUD

--- db/expenses.py
+++ db/expenses.py
@@ -18,13 +18,21 @@ class ExpenseCreate(BaseModel):
     class Config:
         from_attributes = True
 
-def read_db_expense(expense_id: int,session: Session)-> DBExpense:
+class ExpenseUpdate(BaseModel):
+    actualPaymentAmount: float
+    merchantName: str
+    originalAmount: float
+
+class ExpenseDelete(BaseModel):
+    id: int 
+
+def read_db_expense(expense_id: int,session: Session)-> Expense:
     db_expense= session.query(DBExpense).filter(DBExpense.id==expense_id).first()
     if not db_expense:
         raise HTTPException(status_code=404, detail="Expense not found")
     return db_expense
 
-def create_db_expense(expense: ExpenseCreate,session: Session)-> DBExpense:
+def create_db_expense(expense: ExpenseCreate,session: Session)-> Expense:
     try:
         db_item= DBExpense(**expense.model_dump(exclude_none=True))
         session.add(db_item)
@@ -34,3 +42,17 @@ def create_db_expense(expense: ExpenseCreate,session: Session)-> DBExpense:
     except SQLAlchemyError as e:
         session.rollback()
         raise HTTPException(status_code=500, detail=f"Database error: {str(e)}")
+
+def update_db_expense(id: int, expense: ExpenseUpdate, session: Session) -> Expense:
+    db_expense = read_db_expense(id, session)
+    for field, value in expense.model_dump(exclude_none=True).items():
+        setattr(db_expense, field, value)
+    session.commit()
+    session.refresh(db_expense)
+    return db_expense   
+
+def delete_db_expense(id: int, session: Session) -> Expense:
+    db_expense = read_db_expense(id, session)
+    session.delete(db_expense)
+    session.commit()
+    return db_expense   
