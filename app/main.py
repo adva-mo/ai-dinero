573b0cace3aaf78a4f469167e0c684c582bd960c
adva <advamozes@gmail.com>
2024-11-08 21:23:23 +0200

    cors config

--- app/main.py
+++ app/main.py
@@ -1,9 +1,20 @@
+import os
 from fastapi import FastAPI
 from app.routers.expenses import router as expenses_router
+from fastapi.middleware.cors import CORSMiddleware
 from app.routers.users import router as users_router
 from app.routers.categories import router as categories_router
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[os.getenv("FRONTEND_SERVER")],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(expenses_router)
 app.include_router(users_router)
 app.include_router(categories_router)
